0092c449ffe0d996b00a254a7e9600dd912c7a73
Andres Zelaya <andres.zelaya.d@hotmail.com>
2025-03-24 23:33:34 -0300

    Project init

---
+++ backend/src/main/java/architecture_evaluator/backend/ArchitectureEvaluatorBackendApplication.java
@@ -0,0 +1,13 @@
+package architecture_evaluator.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureEvaluatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureEvaluatorBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/architecture_evaluator/backend/ArchitectureEvaluatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package architecture_evaluator.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureEvaluatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
