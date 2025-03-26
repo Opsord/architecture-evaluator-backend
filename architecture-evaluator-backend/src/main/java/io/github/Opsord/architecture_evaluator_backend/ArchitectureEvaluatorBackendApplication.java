cbaa84a4baf510ffb8bf123d581328e6031b31c2
Andres Zelaya <andres.zelaya.d@hotmail.com>
2025-03-26 01:01:59 -0300

    Project re-init

---
+++ architecture-evaluator-backend/src/main/java/io/github/Opsord/architecture_evaluator_backend/ArchitectureEvaluatorBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.Opsord.architecture_evaluator_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureEvaluatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureEvaluatorBackendApplication.class, args);
+	}
+
+}
---
+++ architecture-evaluator-backend/src/test/java/io/github/Opsord/architecture_evaluator_backend/ArchitectureEvaluatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Opsord.architecture_evaluator_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureEvaluatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
