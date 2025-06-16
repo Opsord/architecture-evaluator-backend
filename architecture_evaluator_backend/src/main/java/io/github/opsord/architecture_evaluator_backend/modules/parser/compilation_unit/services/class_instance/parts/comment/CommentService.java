8199661868574b664f7bd07da33cc6c10663e794
Andres Zelaya <andres.zelaya.d@hotmail.com>
2025-06-15 20:26:15 -0400

    Deactivated unused code (maybe useful later)

--- architecture_evaluator_backend/src/main/java/io/github/opsord/architecture_evaluator_backend/modules/parser/compilation_unit/services/class_instance/parts/comment/CommentService.java
+++ architecture_evaluator_backend/src/main/java/io/github/opsord/architecture_evaluator_backend/modules/parser/compilation_unit/services/class_instance/parts/comment/CommentService.java
@@ -1,19 +1,19 @@
 // CommentService.java
-package io.github.opsord.architecture_evaluator_backend.modules.parser.compilation_unit.services.class_instance.parts.comment;
-
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
-public class CommentService {
-
-    public List<String> getComments(ClassOrInterfaceDeclaration classOrInterfaceDeclaration) {
-        List<String> comments = new ArrayList<>();
-        CommentVisitor visitor = new CommentVisitor();
-        classOrInterfaceDeclaration.accept(visitor, comments);
-        return comments;
-    }
-}
+//package io.github.opsord.architecture_evaluator_backend.modules.parser.compilation_unit.services.class_instance.parts.comment;
+//
+//import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Service
+//public class CommentService {
+//
+//    public List<String> getComments(ClassOrInterfaceDeclaration classOrInterfaceDeclaration) {
+//        List<String> comments = new ArrayList<>();
+//        CommentVisitor visitor = new CommentVisitor();
+//        classOrInterfaceDeclaration.accept(visitor, comments);
+//        return comments;
+//    }
+//}
--- architecture_evaluator_backend/src/main/java/io/github/opsord/architecture_evaluator_backend/modules/parser/compilation_unit/services/class_instance/parts/generic_usage/GenericUsageService.java
+++ architecture_evaluator_backend/src/main/java/io/github/opsord/architecture_evaluator_backend/modules/parser/compilation_unit/services/class_instance/parts/generic_usage/GenericUsageService.java
@@ -1,20 +1,20 @@
 // GenericUsageService.java
-package io.github.opsord.architecture_evaluator_backend.modules.parser.compilation_unit.services.class_instance.parts.generic_usage;
-
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import io.github.opsord.architecture_evaluator_backend.modules.parser.compilation_unit.instances.class_instance.parts.GenericUsageInstance;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
-public class GenericUsageService {
-
-    public List<GenericUsageInstance> getGenericUsages(ClassOrInterfaceDeclaration classOrInterfaceDeclaration) {
-        List<GenericUsageInstance> genericUsages = new ArrayList<>();
-        GenericUsageVisitor visitor = new GenericUsageVisitor();
-        classOrInterfaceDeclaration.accept(visitor, genericUsages);
-        return genericUsages;
-    }
-}
+//package io.github.opsord.architecture_evaluator_backend.modules.parser.compilation_unit.services.class_instance.parts.generic_usage;
+//
+//import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+//import io.github.opsord.architecture_evaluator_backend.modules.parser.compilation_unit.instances.class_instance.parts.GenericUsageInstance;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Service
+//public class GenericUsageService {
+//
+//    public List<GenericUsageInstance> getGenericUsages(ClassOrInterfaceDeclaration classOrInterfaceDeclaration) {
+//        List<GenericUsageInstance> genericUsages = new ArrayList<>();
+//        GenericUsageVisitor visitor = new GenericUsageVisitor();
+//        classOrInterfaceDeclaration.accept(visitor, genericUsages);
+//        return genericUsages;
+//    }
+//}
